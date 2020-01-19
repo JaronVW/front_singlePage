f1609d18c66d50d265d1b923800146e1c99c6ed7
Jaron <Jaronvwell@gmail.com>
2020-01-19 12:38:46 +0100

    oeps vergeten to comitten

---
+++ ajax5.php
@@ -0,0 +1,52 @@
+<?php
+
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+// show errors
+
+$db_hostname = "localhost";
+$db_username = 'ajas_83817';
+$db_password = 'imZt36_3';
+$db_name = 'Ajax';
+
+$mysqliConnect = mysqli_connect($db_hostname, $db_username, $db_password, $db_name);
+if (!$mysqliConnect) 
+{
+    
+    echo "connectie naar database mislukt";
+    echo "Erno" . mysqli_connect_error() . "<br>";
+    echo "Error" . mysqli_connect_error() . "<br>";
+    exit;
+
+}
+else
+{
+    function todoList() {
+        $invoer= htmlentities($_POST["invoer"], ENT_QUOTES);
+
+        $result = mysqli_query($mysqliConnect, "INSERT INTO todo (title)
+        VALUES (' {$invoer }')");
+        if ($result) 
+        {
+            echo "OK";
+        } 
+        else 
+        { 
+            echo "FOUT";
+        } 
+    }
+
+    function todoAdd() {
+        $data = [];
+        $result = mysqli_query($mysqliConnect, "SELECT * FROM todo");
+           
+        while ($row = mysqli_fetch_array($result)) 
+        {
+            $data[] = $row;
+        }
+        echo json_encode($data);
+    }
+
+}
